33e84ed86bb523fc93d0ab1dc744b17473644f7f
gabriel <ggarciaseco@gmail.com>
2018-10-13 10:30:18 +0200

    Updates

---
+++ component---src-pages-404-tsx-eeeec46f9c7b5c9b42b1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{134:function(e,t,n){"use strict";n.r(t);var u=n(0);t.default=function(){return u.createElement("div",null,u.createElement("h1",null,"NOT FOUND"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-eeeec46f9c7b5c9b42b1.js.map
