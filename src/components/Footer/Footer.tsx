69e2d809b71e8573109a20dff6d0eeaf9465f91e
gabriel <ggarciaseco@gmail.com>
2018-10-14 12:02:03 +0200

    Add ; to css file

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import FontAwesome from 'react-fontawesome';
+import * as FontAwesome from 'react-fontawesome';
 import { Container, List } from './Footer.style';
 
 const Footer = () => (
@@ -9,15 +9,15 @@ const Footer = () => (
         <a href="https://twitter.com/GGarciaSeco10/" target="_blank">
           <FontAwesome name="twitter"/>
         </a>
-      </li>
+      </li> 
       <li>
         <a href="http://es.linkedin.com/in/gabrielgarciaseco" target="_blank">
-        <FontAwesome name="linkedin"/>
+          <FontAwesome name="linkedin"/>
         </a>
       </li>
       <li>
         <a href="https://github.com/gabrielseco" target="_blank">
-        <FontAwesome name="github"/>
+          <FontAwesome name="github"/>
         </a>
       </li>
     </List>
