438d0257f213d4386e53dde2c23122d449807ea3
gabriel <ggarciaseco@gmail.com>
2018-10-21 11:12:44 +0200

    Add Hover in the social icons

--- src/components/Footer/Footer.style.ts
+++ src/components/Footer/Footer.style.ts
@@ -3,22 +3,35 @@ import styled from 'styled-components';
 export const Container = styled.footer`
   background: #242526;
   display: flex;
+  height: 6.25em;
   padding: 1.875em;
 `;
 
 export const List = styled.ul`
+  align-items: center;
   display: flex;
   justify-content: flex-end;
   list-style: none;
+  padding: 0;
   width: 100%;
 
   li {
+    border-left: 1px solid #4b4b4b;
     display: inline-block;
-    padding: 0 2em;
   }
 
-  li a {
-    color: #fff;
-    text-decoration: none;
+  li:first-child {
+    border: 0;
+  }
+`;
+
+export const IconLink = styled.a<{ hoverColor: string }>`
+  color: ${props => props.theme.colors.white};
+  font-size: 0.875rem;
+  padding: 0 1.0625rem;
+  text-decoration: none;
+
+  &:hover {
+    color: ${props => props.hoverColor};
   }
 `;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,24 +1,24 @@
 import * as React from 'react';
 import * as FontAwesome from 'react-fontawesome';
-import { Container, List } from './Footer.style';
+import { Container, List, IconLink } from './Footer.style';
 
 const Footer = () => (
   <Container>
     <List>
       <li>
-        <a href="https://twitter.com/GGarciaSeco10/" target="_blank">
+        <IconLink hoverColor="#55acee" href="https://twitter.com/GGarciaSeco10/" target="_blank">
           <FontAwesome name="twitter"/>
-        </a>
+        </IconLink>
       </li> 
       <li>
-        <a href="http://es.linkedin.com/in/gabrielgarciaseco" target="_blank">
+        <IconLink hoverColor="#0e76a8" href="http://es.linkedin.com/in/gabrielgarciaseco" target="_blank">
           <FontAwesome name="linkedin"/>
-        </a>
+        </IconLink>
       </li>
       <li>
-        <a href="https://github.com/gabrielseco" target="_blank">
+        <IconLink hoverColor="#999" href="https://github.com/gabrielseco" target="_blank">
           <FontAwesome name="github"/>
-        </a>
+        </IconLink>
       </li>
     </List>
   </Container>
