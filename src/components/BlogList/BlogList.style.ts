355b6f51cb2d3fe405fd5c2f4c2b01c6447a3276
gabriel <ggarciaseco@gmail.com>
2018-10-30 22:01:04 +0100

    BlogList remove rem fn

--- src/components/BlogList/BlogList.style.ts
+++ src/components/BlogList/BlogList.style.ts
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
-import { rem } from './../../utils/styled';
 
 export const Article = styled.article`
-  padding: ${rem(5.625)} 0 ${rem(3.625)};
+  padding: 9rem 0 5.8rem;
 
   &:nth-child(odd) {
     background: #f9f9f9;
@@ -11,7 +10,7 @@ export const Article = styled.article`
 
 export const Wrapper = styled.div`
   margin: 0 auto;
-  max-width: ${rem(56.25)};
+  max-width: 90rem;
   width: 90%;
 `;
 
@@ -22,24 +21,24 @@ export const List = styled.ul`
   margin: 0;
   padding: 0;
   list-style: none;
-  line-height: ${rem(1.75)};
+  line-height: 2.8rem;
 `;
 
 export const DateTime = styled.a`
   color: #b5b5b5;
-  font-size: ${rem(0.9375)};
+  font-size: 1.5rem;
   font-family: ${props => props.theme.fonts.montserrat};
   letter-spacing: -1px;
-  padding: 0 ${rem(0.5)};
+  padding: 0 0.8rem;
 `;
 
 export const Title = styled.h2`
   font-family: ${props => props.theme.fonts.varela};
-  font-size: ${rem(3)};
+  font-size: 4.8rem;
   font-weight: 600;
   letter-spacing: -1px;
-  line-height: ${rem(3.875)};
-  margin: ${rem(2.8125)} 0;
+  line-height: 6.2rem;
+  margin: 4.5rem 0;
 
   a {
     color: #373737;
@@ -50,9 +49,9 @@ export const Excerpt = styled.p`
   color: #62646c;
   color: rgba(35, 37, 40, 0.7);
   font-family: ${props => props.theme.fonts.noto};
-  font-size: ${rem(1.1875)};
+  font-size: 1.9rem;
   font-weight: 400;
   overflow: hidden;
-  margin: ${rem(2.5)} 0;
-  line-height: ${rem(2.8125)};
+  margin: 4rem 0;
+  line-height: 4.5rem;
 `;
