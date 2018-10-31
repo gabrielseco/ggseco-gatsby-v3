3899cb58c360e7ff7ba7ae78f1a95c0981482025
gabriel <ggarciaseco@gmail.com>
2018-10-31 06:35:06 +0100

    BlogPost remove rem fn

--- src/templates/BlogPost.style.ts
+++ src/templates/BlogPost.style.ts
@@ -1,32 +1,31 @@
 import styled from 'styled-components';
-import { rem } from './../utils/styled';
 
 export const Title = styled.h1`
   color: ${props => props.theme.colors.white};
-  font-size: ${rem(3)};
+  font-size: 4.8rem;
   font-family: ${props => props.theme.fonts.varela};
   font-weight: 600;
-  line-height: ${rem(3.875)};
-  margin: ${rem(2.1875)} 0;
+  line-height: 6.2rem;
+  margin: 3.5rem 0;
 `;
 
 export const Subtitle = styled.p`
   color: #939393;
   letter-spacing: -1px;
-  font-size: ${rem(1.5)};
+  font-size: 2.4rem;
   font-family: ${props => props.theme.fonts.varela};
-  line-height: ${rem(2.375)};
-  margin: ${rem(2.1875)} 0;
+  line-height: 3.8rem;
+  margin: 3.5rem 0;
 `;
 
 export const Section = styled.section`
   color: rgba(35, 37, 40, 0.7);
   font-family: ${props => props.theme.fonts.noto};
-  font-size: ${rem(1.1875)};
+  font-size: 1.9rem;
   font-weight: 400;
-  line-height: ${rem(2.1875)};
-  margin: ${rem(5)} auto;
-  max-width: ${rem(56.25)};
+  line-height: 3.5rem;
+  margin: 8rem auto;
+  max-width: 90rem;
 
   a {
     border-bottom: 1px solid #d4d5de;
@@ -36,25 +35,25 @@ export const Section = styled.section`
   h3 {
     color: #b2b2b2;
     font-family: 'Varela Round', 'Arial', Sans-Serif;
-    font-size: ${rem(1.75)};
+    font-size: 2.8rem;
     font-weight: 400;
     letter-spacing: -1px;
-    line-height: ${rem(2.75)};
-    margin: ${rem(3.125)} 0 ${rem(1.875)};
+    line-height: 4.4rem;
+    margin: 5rem 0 3rem;
   }
 
   li {
-    margin: 0 0 ${rem(1.25)};
+    margin: 0 0 2rem;
     padding: 0 2.5%;
   }
 
   p {
     color: rgba(35, 37, 40, 0.7);
     font-family: ${props => props.theme.fonts.noto};
-    font-size: ${rem(1.1875)};
+    font-size: 1.9rem;
     font-weight: 400;
-    line-height: ${rem(2.1875)};
-    margin: ${rem(2.8125)} 0;
+    line-height: 3.5rem;
+    margin: 4.5rem 0;
     padding: 0;
   }
 `;
