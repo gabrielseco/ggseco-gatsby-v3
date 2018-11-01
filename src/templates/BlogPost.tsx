fc09ed3375a4dcb9f2755697ed1e39bb7c4491bc
gabriel <ggarciaseco@gmail.com>
2018-11-01 22:42:06 +0100

    Change to netlify to deploy

--- src/templates/BlogPost.tsx
+++ src/templates/BlogPost.tsx
@@ -16,7 +16,7 @@ import './../layouts/index.css';
 
 const getPathImagesBlog = (url: string) => {
   return process.env.NODE_ENV === 'production' // TODO: Delete this with custom domain
-    ? `https://gabrielseco.github.io${url}`
+    ? `https://ggseco.com${url}`
     : url;
 };
 
