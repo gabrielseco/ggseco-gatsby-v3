b487050f204ce063267e87787538998ac69f3dd8
gabriel <ggarciaseco@gmail.com>
2018-11-10 20:47:42 +0100

    Add only the recapcha component

--- src/components/FormContact/FormContact.tsx
+++ src/components/FormContact/FormContact.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import axios from 'axios';
-import { loadReCaptcha } from 'react-recaptcha-v3';
+import { loadReCaptcha, ReCaptcha } from 'react-recaptcha-v3';
 import { Alert, AlertEnum } from './../../components';
 import {
   Form,
@@ -151,6 +151,11 @@ export default class FormContact extends React.Component<any, IState> {
         <ButtonContainer>
           <Button type="submit">Enviar</Button>
         </ButtonContainer>
+        <ReCaptcha
+          sitekey={siteKey}
+          action="contact"
+          verifyCallback={this.verifyCallback}
+        />
       </Form>
     );
   }
