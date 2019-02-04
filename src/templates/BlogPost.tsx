c628f4c2d8430316f6246a79b48e10a7e3c26bbe
gabriel <ggarciaseco@gmail.com>
2019-02-04 01:34:01 +0100

    Tags header seo give some dynamism

--- src/templates/BlogPost.tsx
+++ src/templates/BlogPost.tsx
@@ -1,40 +1,31 @@
-import * as React from 'react';
-import Helmet from 'react-helmet';
-import { ThemeProvider } from 'styled-components';
-import { graphql } from 'gatsby';
-import theme from './../theme';
-import {
-  Cover,
-  CoverContainer,
-  Header,
-  Footer,
-  Separator,
-  Tag,
-} from './../components';
-import { Title, Subtitle, Section } from './BlogPost.style';
-import './../layouts/index.css';
+import * as React from "react";
+import Helmet from "react-helmet";
+import { ThemeProvider } from "styled-components";
+import { graphql } from "gatsby";
+import theme from "./../theme";
+import { Cover, CoverContainer, Header, Footer, Separator, Tag } from "./../components";
+import { Title, Subtitle, Section } from "./BlogPost.style";
+import "./../layouts/index.css";
 
 const getPathImagesBlog = (url: string) => {
-  return process.env.NODE_ENV === 'production' // TODO: Delete this with custom domain
+  return process.env.NODE_ENV === "production" // TODO: Delete this with custom domain
     ? `https://ggseco.com${url}`
     : url;
 };
 
 const Template = (props: any) => {
-  const image = getPathImagesBlog(
-    props.data.markdownRemark.frontmatter.featured_image.publicURL,
-  );
+  const image = getPathImagesBlog(props.data.markdownRemark.frontmatter.featured_image.publicURL);
   return (
     <ThemeProvider theme={theme}>
       <div>
         <Helmet
-          title="Gabriel García Seco - Desarrollo Web | Music &amp; More"
+          title={props.data.markdownRemark.frontmatter.title}
           meta={[
-            { name: 'description', content: 'Desarrollo Web | Music & More' },
+            { name: "description", content: props.data.markdownRemark.frontmatter.description },
             {
-              name: 'keywords',
-              content: 'React, Javascript, Desarrollo Web, CSS',
-            },
+              name: "keywords",
+              content: "React, Javascript, Desarrollo Web, CSS"
+            }
           ]}
         />
         <Cover image={image}>
@@ -42,15 +33,11 @@ const Template = (props: any) => {
           <CoverContainer center>
             <Tag>{props.data.markdownRemark.frontmatter.tags[0]}</Tag>
             <Title>{props.data.markdownRemark.frontmatter.title}</Title>
-            <Subtitle>
-              {props.data.markdownRemark.frontmatter.subtitle}
-            </Subtitle>
+            <Subtitle>{props.data.markdownRemark.frontmatter.subtitle}</Subtitle>
             <Separator />
           </CoverContainer>
         </Cover>
-        <Section
-          dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
-        />
+        <Section dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }} />
         <Footer />
       </div>
     </ThemeProvider>
